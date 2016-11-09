fc4e3e6411a58a3befd768fbea48b70a2c906460
bmaythrowback <bmay@throwbackcorp.com>
2016-11-09 16:45:41 -0500

    added basic player functionality and basic materials

---
+++ NarlesBarkley/Assets/Narles/Scripts/checkTongueHit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkTongueHit : MonoBehaviour {
+
+    private extendTongue script;
+    // Use this for initialization
+	void Start ()
+    {
+        script = GameObject.FindObjectOfType(typeof(extendTongue)) as extendTongue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Platform")
+        {           
+            Debug.Log("Hit");
+            script.MoveUp();            
+        }
+        
+    }
+}
---
+++ NarlesBarkley/Assets/Narles/Scripts/extendTongue.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class extendTongue : MonoBehaviour {
+        
+    public Rigidbody2D rb;
+    public GameObject playerTongue;
+    private bool hitPlatform;
+
+    // Use this for initialization
+    void Start () {        
+        rb = GetComponent<Rigidbody2D>();
+        hitPlatform = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Extend the tongue");
+            StartCoroutine(activateTongue());            
+        }        
+    }
+        
+    void Update ()
+    {
+        if (hitPlatform)
+        {
+            Debug.Log("Move the player");
+        }
+
+    }
+
+    void MoveToNextPlatform()
+    {
+        transform.Translate(0, 2.0f, 0);
+    }
+    
+    IEnumerator activateTongue() {
+        playerTongue.SetActive(true);   
+        yield return new WaitForSeconds(0.5f);       
+        playerTongue.SetActive(false);
+    }
+
+    public void MoveUp()
+    {
+        MoveToNextPlatform();
+    }
+}
